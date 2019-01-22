e77539c0b133e287d03d36e077aea29ada982789
Shalitha Suranga <shalithasuranga@gmail.com>
2019-01-22 05:14:05 -0800

    Change project to ts support

---
+++ src/components/AppleComponent.vue
@@ -0,0 +1,7 @@
+<template src="./AppleComponent.html"></template>
+
+<script lang="ts" src="./AppleComponent.ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
